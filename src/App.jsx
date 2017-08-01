289798f7dbe18aef654d322c2abf86358d32e274
felipe <felipe.leonco@gmail.com>
2017-08-01 17:20:04 +0300

    final

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import Clock from './Clock';
+import './App.css'
+import {Form, FormControl, Button} from 'react-bootstrap'
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            deadline:'December 24, 2017',
+            newDeadLine:''
+        }
+    }
+
+    changeDeadLine(){
+         this.setState({deadline:this.state.newDeadLine})
+    }
+
+    render() {
+        return (
+            <div className='App'>
+                <div className='App-title'> Countdown to {this.state.deadline} </div>
+                <Clock
+                    deadline={this.state.deadline}
+                 />
+                <Form >
+                <FormControl 
+                    className="DeadLine-input"
+                    placeholder='New Date'
+                    onChange={event => this.setState({newDeadLine: event.target.value})}
+                    />
+                <Button onClick={() => this.changeDeadLine()}>Submit </Button>
+                </Form>
+         </div>
+        )
+
+    }
+}
+
+export default App;
